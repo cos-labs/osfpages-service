7342d8c2b7bbd9362ad6d6fc12bdd460194fafd0
Ryan Mason <Rytiggy@gmail.com>
2017-08-24 11:34:19 -0400

    add postgres

---
+++ service/settings/production.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+import dj_database_url
+
+from .settings import *
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+# CSRF_TRUSTED_ORIGINS = ['dev-labs-2.cos.io']
+
+DEBUG=True
+SECRET_KEY = os.environ['SECRET_KEY']
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATIC_URL = 'https://cos-labs.github.io/collections-service/'
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
+
+try:
+    from .local import *
+except ImportError:
+    print("Attention: No Local Settings Defined.")
+    pass
