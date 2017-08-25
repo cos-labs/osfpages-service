0f9741cd758414cb27616dceb5aaaef2bbda884f
Ryan Mason <Rytiggy@gmail.com>
2017-08-25 10:44:21 -0400

    remove unpublished_page_data as a required field

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 
 
 class HomeSerializer(serializers.HyperlinkedModelSerializer):
+    unpublished_page_data = serializers.TextField(required=False)
     class Meta:
         model = Home
-        fields = ('guid', 'page_data', 'unpublished_page_data')
+        fields = ('page_data', 'unpublished_page_data')
