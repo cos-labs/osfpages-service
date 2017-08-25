a2fbcb360e3f9e7ac287101456daa463034ff0c6
Ryan Mason <Rytiggy@gmail.com>
2017-08-25 10:53:40 -0400

    update serializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 
 class HomeSerializer(serializers.HyperlinkedModelSerializer):
-    unpublished_page_data = serializers.CharField(required=False)
+    unpublished_page_data = serializers.CharField(required=False, allow_null=True, allow_blank=True)
     class Meta:
         model = Home
         fields = ('page_data', 'unpublished_page_data')
