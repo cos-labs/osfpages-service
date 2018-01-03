e39f7618e6bc140b38c39cf4a53d0d5d9022679e
Ryan Mason <Rytiggy@gmail.com>
2018-01-03 11:27:52 -0500

    update to fix bugs

--- api/serializers.py
+++ api/serializers.py
@@ -6,7 +6,4 @@ class HomeSerializer(serializers.HyperlinkedModelSerializer):
     unpublished_page_data = serializers.CharField(required=False, allow_null=True, allow_blank=True)
     class Meta:
         model = Home
-        fields = ['guid',
-                  'page_data',
-                  'unpublished_page_data',
-                  'meta_data']
+        fields = ('guid', 'page_data', 'unpublished_page_data', 'meta_data')
