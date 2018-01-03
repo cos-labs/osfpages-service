5e48313551f0d1186ad4e0e38d7eee1a125b6688
Ryan Mason <Rytiggy@gmail.com>
2018-01-03 10:57:42 -0500

    update  view / sterilizer / model to have metadata

--- api/serializers.py
+++ api/serializers.py
@@ -6,4 +6,7 @@ class HomeSerializer(serializers.HyperlinkedModelSerializer):
     unpublished_page_data = serializers.CharField(required=False, allow_null=True, allow_blank=True)
     class Meta:
         model = Home
-        fields = ('guid', 'page_data', 'unpublished_page_data')
+        fields = ['guid',
+                  'page_data',
+                  'unpublished_page_data',
+                  'meta_data']
